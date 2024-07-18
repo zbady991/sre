f376418622ad7eef2548af293a88490ec1049bd5
Alaa-eddine K. <alaa.eddine@gmail.com>
2024-07-18 18:13:34 +0200

    support async process in TemplateString Add parseTeamKeys shortcut for vault key parsing

--- packages/core/src/Core/Connector.class.ts
+++ packages/core/src/Core/Connector.class.ts
@@ -4,11 +4,41 @@ const console = createLogger('Connector');
 
 export abstract class Connector {
     public abstract name: string;
+    public started = false;
+    private _readyPromise: Promise<boolean>;
+
     public async start() {
         console.info(`Starting ${this.name} connector ...`);
+        this.started = true;
     }
 
     public async stop() {
         console.info(`Stopping ${this.name} connector ...`);
+    }
+
+    public ready() {
+        if (!this._readyPromise) {
+            this._readyPromise = new Promise((resolve) => {
+                let maxWait = 10000;
+                const tick = 100;
+                if (this.started) {
+                    resolve(true);
+                } else {
+                    const interval = setInterval(() => {
+                        if (this.started) {
+                            clearInterval(interval);
+                            resolve(true);
+                        }
+
+                        maxWait -= tick;
+                        if (maxWait <= 0) {
+                            clearInterval(interval);
+                            resolve(false);
+                        }
+                    }, tick);
+                }
+            });
+        }
+        return this._readyPromise;
     }
 }
---
+++ packages/core/src/subsystems/Security/Vault.service/Vault.helper.ts
@@ -0,0 +1,27 @@
+import { ConnectorService } from '@sre/Core/ConnectorsService';
+import { AccessCandidate } from '../AccessControl/AccessCandidate.class';
+
+export class VaultHelper {
+    static async getTeamKey(key: string, teamId: string): Promise<string> {
+        const vaultConnector = ConnectorService.getVaultConnector();
+        return await vaultConnector.user(AccessCandidate.team(teamId)).get(key);
+    }
+
+    static async getUserKey(key: string, userId: string): Promise<string> {
+        const vaultConnector = ConnectorService.getVaultConnector();
+        const accountConnector = ConnectorService.getAccountConnector();
+
+        const teamId = await accountConnector.getCandidateTeam(AccessCandidate.user(userId));
+
+        return await vaultConnector.user(AccessCandidate.team(teamId)).get(key);
+    }
+
+    static async getAgentKey(key: string, agentId: string): Promise<string> {
+        const vaultConnector = ConnectorService.getVaultConnector();
+        const accountConnector = ConnectorService.getAccountConnector();
+
+        const teamId = await accountConnector.getCandidateTeam(AccessCandidate.agent(agentId));
+
+        return await vaultConnector.user(AccessCandidate.team(teamId)).get(key);
+    }
+}
