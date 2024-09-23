a7ec5e2c50236b54523d2f23960c51fef4a2ee4a
Forhad Hosain <forhad@smythos.com>
2024-09-24 00:01:38 +0600

    fix tokens for context window

--- packages/core/src/subsystems/MemoryManager/LLMContext.ts
+++ packages/core/src/subsystems/MemoryManager/LLMContext.ts
@@ -47,7 +47,7 @@ export class LLMContext {
     public async getContextWindow(maxTokens: number, maxOutputTokens: number = 256): Promise<any[]> {
         //TODO: handle non key accounts (limit tokens)
         // const maxModelContext = this._llmHelper?.modelInfo?.keyOptions?.tokens || this._llmHelper?.modelInfo?.tokens || 256;
-        const maxModelContext = await this._llmHelper.TokenManager().getAllowedCompletionTokens(this._model, true);
+        const maxModelContext = await this._llmHelper.TokenManager().getAllowedContextTokens(this._model, true);
         let maxInputContext = Math.min(maxTokens, maxModelContext);
 
         if (maxInputContext + maxOutputTokens > maxModelContext) {
