42f6c85d1c4fc551361242e002c3f012c435f37f
Alaa-eddine K. <alaa.eddine@gmail.com>
2024-10-09 12:59:11 +0200

    update

--- packages/core/src/subsystems/LLMManager/LLM.service/connectors/AnthropicAI.class.ts
+++ packages/core/src/subsystems/LLMManager/LLM.service/connectors/AnthropicAI.class.ts
@@ -263,6 +263,7 @@ export class AnthropicAIConnector extends LLMConnector {
                         {
                             type: 'text',
                             text: messageCreateArgs.system,
+                            //cache_control: { type: 'ephemeral' }, //TODO: @Forhad check this
                         },
                     ];
                 }
