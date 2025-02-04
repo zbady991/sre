c016e9ed8ea9e127fa3ac05052ab3c923b586781
Zubair <zubair@smythos.com>
2025-02-04 22:06:33 +0500

    fixed failedResults typo for web scrape component

--- packages/core/src/Components/WebScrape.class.ts
+++ packages/core/src/Components/WebScrape.class.ts
@@ -37,7 +37,7 @@ export default class WebScrape extends Component {
             });
 
             Output = { results: response.data.results };
-            _error = response.data.failedResults?.length ? JSON.stringify(response.data.failedResults) : undefined;
+            _error = response.data.failed_results?.length ? JSON.stringify(response.data.failed_results) : undefined;
             this.reportUsage(response?.data?.results?.length, agent.id);
             return { ...Output, _error, _debug: logger.output };
         } catch (err: any) {
