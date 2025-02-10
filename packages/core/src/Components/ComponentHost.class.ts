befb06954d0becfb0d4fbe542802499c637a33d8
Alaa-eddine K. <alaa.eddine@gmail.com>
2025-02-10 11:55:40 +0100

    Component Host support

---
+++ packages/core/src/Components/ComponentHost.class.ts
@@ -0,0 +1,38 @@
+import Component from './Component.class';
+import Agent from '@sre/AgentManager/Agent.class';
+import { ConnectorService } from '@sre/Core/ConnectorsService';
+import { AccessCandidate } from '@sre/Security/AccessControl/AccessCandidate.class';
+
+/**
+ * This component allows running components that are not natively shiped with SRE
+ * it can be used to extend SRE components by registering custom component using ComponentService
+ */
+export default class ComponentHost extends Component {
+    protected configSchema = null;
+    constructor() {
+        super();
+    }
+    init() {}
+
+    async process(input, config, agent: Agent) {
+        await super.process(input, config, agent);
+        //let debugLog = agent.agentRuntime?.debug ? [] : undefined;
+        const logger = this.createComponentLogger(agent, config.name);
+
+        try {
+            const componentName = config.data._component;
+            const componentConnector = ConnectorService.getComponentConnector(componentName);
+
+            const component = await componentConnector.user(AccessCandidate.agent(agent.id)).get(componentName);
+
+            if (!component) {
+                logger.debug(`Component ${componentName} not found`);
+                return { _error: `Component ${componentName} not found`, _debug: logger.output };
+            }
+
+            return await component.process(input, config, agent);
+        } catch (error) {
+            return { _error: error.message, _debug: logger.output };
+        }
+    }
+}
---
+++ packages/core/src/subsystems/AgentManager/Component.service/connectors/LocalComponentConnector.class.ts
@@ -0,0 +1,42 @@
+import { ConnectorService } from '@sre/Core/ConnectorsService';
+import { Logger } from '@sre/helpers/Log.helper';
+
+import { AccessRequest } from '@sre/Security/AccessControl/AccessRequest.class';
+import { ACL } from '@sre/Security/AccessControl/ACL.class';
+import { SecureConnector } from '@sre/Security/SecureConnector.class';
+import { IAccessCandidate, TAccessLevel, TAccessRole } from '@sre/types/ACL.types';
+
+import { ComponentConnector } from '../ComponentConnector';
+
+const console = Logger('LocalComponentConnector');
+export class LocalComponentConnector extends ComponentConnector {
+    public name: string = 'LocalComponentConnector';
+    private components: any = {};
+
+    constructor() {
+        super();
+    }
+
+    @SecureConnector.AccessControl
+    protected async register(acRequest: AccessRequest, componentName: string, componentInstance: any) {
+        this.components[componentName] = componentInstance;
+    }
+
+    @SecureConnector.AccessControl
+    protected async get(acRequest: AccessRequest, componentName: string) {
+        return this.components[componentName];
+    }
+
+    public async getResourceACL(resourceId: string, candidate: IAccessCandidate) {
+        const accountConnector = ConnectorService.getAccountConnector();
+
+        const teamId = await accountConnector.getCandidateTeam(candidate);
+
+        const acl = new ACL();
+
+        //Grant read access by default
+        acl.addAccess(TAccessRole.Team, teamId, TAccessLevel.Read).addAccess(candidate.role, candidate.id, TAccessLevel.Read);
+
+        return acl;
+    }
+}
