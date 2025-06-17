0c6304cb796cf421c5f57b2ff0567b967f6d96ef
Alaa-eddine K. <alaa.eddine@gmail.com>
2025-06-17 18:11:57 +0200

    update postinstall script

--- packages/cli/src/scripts/postinstall.ts
+++ packages/cli/src/scripts/postinstall.ts
@@ -1,13 +1,14 @@
 import chalk from 'chalk';
 
 console.log('');
-console.log(chalk.green('Thank you for installing SmythOS CLI!'));
+console.log(chalk.green('SmythOS CLI is installed!'));
 console.log('');
 console.log(`You can now use the ${chalk.cyan('sre')} command in your terminal.`);
 console.log('');
-console.log('To see all available commands, run:');
+console.log('You can invoke it by typing:');
 console.log(chalk.cyan('  sre --help'));
+console.log('to get help.');
 console.log('');
 console.log('For more information and documentation, visit:');
-console.log(chalk.underline('https://github.com/smythos/sre'));
+console.log(chalk.underline('https://github.com/SmythOS/sre/tree/main/packages/cli#readme'));
 console.log('');
